c7588d8784e9c8a77430cdf97211a34d74901be5
Lakshya Garg <lakshyagarg708@gmail.com>
2026-05-08 18:51:46 +0530

    Time: 431 ms (84.87%), Space: 335.2 MB (43.91%) - LeetHub

---
+++ 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int minJumps(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return 0;
+
+        int mx = *max_element(nums.begin(), nums.end());
+
+        vector<int> spf(mx + 1);
+        for (int i = 0; i <= mx; i++) spf[i] = i;
+
+        for (long long i = 2; i * i <= mx; i++) {
+            if (spf[i] == i) {
+                for (long long j = i * i; j <= mx; j += i) {
+                    if (spf[j] == j) spf[j] = i;
+                }
+            }
+        }
+
+        auto isPrime = [&](int x) {
+            return x >= 2 && spf[x] == x;
+        };
+
+        unordered_map<int, vector<int>> mp;
+
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            while (x > 1) {
+                int p = spf[x];
+                mp[p].push_back(i);
+                while (x % p == 0) x /= p;
+            }
+        }
+
+        vector<int> dist(n, -1);
+        queue<int> q;
+
+        dist[0] = 0;
+        q.push(0);
+
+        while (!q.empty()) {
+            int i = q.front();
+            q.pop();
+
+            if (i == n - 1) return dist[i];
+
+            if (i - 1 >= 0 && dist[i - 1] == -1) {
+                dist[i - 1] = dist[i] + 1;
+                q.push(i - 1);
+            }
+
+            if (i + 1 < n && dist[i + 1] == -1) {
+                dist[i + 1] = dist[i] + 1;
+                q.push(i + 1);
+            }
+
+            if (isPrime(nums[i]) && mp.count(nums[i])) {
+                for (int j : mp[nums[i]]) {
+                    if (dist[j] == -1) {
+                        dist[j] = dist[i] + 1;
+                        q.push(j);
+                    }
+                }
+                mp.erase(nums[i]);
+            }
+        }
+
+        return 0;
+    }
+};
