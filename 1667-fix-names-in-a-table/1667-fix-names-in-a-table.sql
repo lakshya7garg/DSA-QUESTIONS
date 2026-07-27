1e93108cb1c39aa56cde353e52b3ea9ed79d4ff8
Lakshya Garg <lakshyagarg708@gmail.com>
2026-07-27 15:24:02 +0530

    Time: 746 ms (26.63%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,6 +1,6 @@
 SELECT 
     user_id,
-    CONCAT(UPPER(SUBSTRING(name,1,1)),LOWER(SUBSTRING(name,2,LENGTH(name)))) 
+    CONCAT(UPPER(LEFT(name,1)),LOWER(SUBSTRING(name,2))) 
     AS name
 FROM Users
 ORDER BY user_id ASC;
